c3f53bf7aee10ee958b00e101f81b2d7d91764b9
jitiendran <jitiendranjiji2000@gmail.com>
2021-07-13 14:14:32 +0530

    CORS fix

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors);
+app.use(cors());
 
 mongoose.connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.md3ui.mongodb.net/colorgrad`,
